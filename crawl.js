72ad8095517e40586ff3ffe6e68523bd37ba42c1
code.organise.earth <auto@code.organise.earth>
2024-12-07 10:11:22 +0000

    feat: keep query params on keys and deal with race condition edge case

--- crawl.js
+++ crawl.js
@@ -1,5 +1,8 @@
+const crawledList = [];
+
 const crawl = async (url, siteMap = {}) => {
   const urlObj = new URL(url);
+  if (urlObj.pathname === '/') crawledList.length = 0;
 
   // Crawl the site
   const response = await fetch(url);
@@ -15,17 +18,27 @@ const crawl = async (url, siteMap = {}) => {
     (link) => link !== urlObj.pathname
   );
 
-  siteMap[urlObj.pathname] = dedupedLinks;
+  const fullPath = urlObj.href.split('/').reverse()[0];
+  siteMap[`/${fullPath}`] = dedupedLinks;
 
   // Recursively crawl the site
   for (const link of dedupedLinks) {
     if (!siteMap[link]) {
-      console.log(`Crawling ${urlObj.origin}${link}`);
+      console.log(`Crawling ${link} from ${fullPath}`);
+
+      // TODO: refactor this so we don't need to keep a separate 'checklist'
+      // This is needed because we are doing a depth first search
+      // Possibly refactoring to a breadth first search would mitigate this
+      if (crawledList.includes(link)) {
+        continue;
+      } else {
+        crawledList.push(link);
+      }
       await crawl(`${urlObj.origin}${link}`, siteMap);
     }
   }
 
-  return siteMap
+  return siteMap;
 };
 
 module.exports = { crawl };
--- crawl.test.js
+++ crawl.test.js
@@ -2,31 +2,31 @@ const { crawl } = require('./crawl.js');
 require('jest-fetch-mock').enableMocks();
 
 describe('WebCrawler', () => {
-    beforeEach(() => {
-        fetch.resetMocks();
-    });
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
 
-    it('should fetch the page and return the links', async () => {
-        // Arrange
-        const sampleHtml = `
+  it('should fetch the page and return the links', async () => {
+    // Arrange
+    const sampleHtml = `
             <html><body>
                     <a href="/foo">Foo</a>
                     <a href="/bar">Bar</a>
             </body></html>
         `;
-        fetch.mockResponseOnce(sampleHtml);
+    fetch.mockResponseOnce(sampleHtml);
 
-        // Act
-        const result = await crawl('https://www.asdfasdfasdf.com');
+    // Act
+    const result = await crawl('https://www.asdfasdfasdf.com');
 
-        // Assert
-        expect(typeof result).toBe('object');
-        expect(result['/']).toEqual(['/foo', '/bar']);
-    });
+    // Assert
+    expect(typeof result).toBe('object');
+    expect(result['/']).toEqual(['/foo', '/bar']);
+  });
 
-    it('should not include duplicate links or links to itself', async () => {
-        // Arrange
-        const sampleHtml = `
+  it('should not include duplicate links or links to itself', async () => {
+    // Arrange
+    const sampleHtml = `
             <html><body>
                     <a href="/foo">Foo</a>
                     <a href="/">Home</a>
@@ -34,18 +34,18 @@ describe('WebCrawler', () => {
                     <a href="/bar">Bar</a>
             </body></html>
         `;
-        fetch.mockResponseOnce(sampleHtml);
+    fetch.mockResponseOnce(sampleHtml);
 
-        // Act
-        const result = await crawl('https://www.asdfasdfasdf.com');
+    // Act
+    const result = await crawl('https://www.asdfasdfasdf.com');
 
-        // Assert
-        expect(typeof result).toBe('object');
-        expect(result['/']).toEqual(['/foo', '/bar']);
-    });
-    it('should only include internal page links', async () => {
-        // Arrange
-        const sampleHtml = `
+    // Assert
+    expect(typeof result).toBe('object');
+    expect(result['/']).toEqual(['/foo', '/bar']);
+  });
+  it('should only include internal page links', async () => {
+    // Arrange
+    const sampleHtml = `
             <html><body>
                     <a href="/foo">Foo</a>
                     <a href="https://foo.com/bar">Bar</a>
@@ -53,53 +53,71 @@ describe('WebCrawler', () => {
                     <a href="/bar">Bar</a>
             </body></html>
         `;
-        fetch.mockResponseOnce(sampleHtml);
+    fetch.mockResponseOnce(sampleHtml);
 
-        // Act
-        const result = await crawl('https://www.asdfasdfasdf.com');
+    // Act
+    const result = await crawl('https://www.asdfasdfasdf.com');
 
-        // Assert
-        expect(typeof result).toBe('object');
-        expect(result['/']).toEqual(['/foo', '/bar']);
-    });
-    it('should fetch the page and return the links recursively', async () => {
-        // Arrange
-        const rootHtml = `
+    // Assert
+    expect(typeof result).toBe('object');
+    expect(result['/']).toEqual(['/foo', '/bar']);
+  });
+  it('should include query params in the page keys', async () => {
+    // Arrange
+    const sampleHtml = `
+            <html><body>
+                    <a href="/foo">Foo</a>
+                    <a href="/foo?baz=123">Baz</a>
+                    <a href="/bar">Bar</a>
+            </body></html>
+        `;
+    fetch.mockResponseOnce(sampleHtml);
+
+    // Act
+    const result = await crawl('https://www.asdfasdfasdf.com');
+
+    // Assert
+    expect(typeof result).toBe('object');
+    expect(result['/']).toEqual(['/foo', '/foo?baz=123', '/bar']);
+  });
+  it('should fetch the page and return the links recursively', async () => {
+    // Arrange
+    const rootHtml = `
             <html><body>
                     <a href="/foo">Foo</a>
                     <a href="/bar">Bar</a>
             </body></html>
         `;
-        fetch.mockResponseOnce(rootHtml); // 2 links
-        const fooHtml = `
+    fetch.mockResponseOnce(rootHtml); // 2 links
+    const fooHtml = `
             <html><body>
                     <a href="/baz">Baz</a>
             </body></html>
         `;
-        fetch.mockResponseOnce(fooHtml); // 1 link
-        const barHtml = `
+    fetch.mockResponseOnce(fooHtml); // 1 link
+    const barHtml = `
             <html><body>
                     <a href="https://www.example.com">external</a>
                     <a href="#blah">ignore</a>
             </body></html>
         `;
-        fetch.mockResponseOnce(barHtml); // no internal links
-        const bazHtml = `
+    fetch.mockResponseOnce(barHtml); // no internal links
+    const bazHtml = `
             <html><body>
             </body></html>
         `;
-        fetch.mockResponseOnce(bazHtml); // no links
+    fetch.mockResponseOnce(bazHtml); // no links
 
-        // Act
-        const result = await crawl('https://www.asdfasdfasdf.com');
+    // Act
+    const result = await crawl('https://www.asdfasdfasdf.com');
 
-        // Assert
-        expect(typeof result).toBe('object');
-        expect(result).toEqual({
-            '/': ['/foo', '/bar'],
-            '/foo': ['/baz'],
-            '/bar': [],
-            '/baz': []
-        });
+    // Assert
+    expect(typeof result).toBe('object');
+    expect(result).toEqual({
+      '/': ['/foo', '/bar'],
+      '/foo': ['/baz'],
+      '/bar': [],
+      '/baz': [],
     });
+  });
 });
